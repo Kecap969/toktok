12fa46a91be743c8a9597213434094579ebad6ac
Kecap969 <kecapabecemanis@gmail.com>
2026-09-04 21:24:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@
 // PENTING: batasi API key ini di Google Cloud Console -> Credentials ->
 // "Application restrictions" -> HTTP referrers -> isi domain GitHub Pages
 // Anda, contoh: username.github.io/*
-const API_KEY = "AIzaSyBxmHrrGOA_TseA3OtthWtXkjsda_vVtfQ";
+const API_KEY = "AIzaSyB8MY-5lLPOirCFvXO8qEwHgY5zntv0m4c";
 
 // ID folder Google Drive yang isinya video (folder harus di-share:
 // "Anyone with the link" -> Viewer)
 // Cara ambil ID: buka folder di Drive, lihat URL-nya
 // https://drive.google.com/drive/folders/INI_ID_NYA
-const FOLDER_ID = "1KAo3iiv2dHyZPh8R4fVIbT1ei7b0lPY1";
+const FOLDER_ID = "1RjxjqHRT6X9sU8rH87pfzz6hr-VlKet-";
 
 // Nama tampilan default untuk uploader (karena Drive publik tidak
 // menyimpan info "siapa yang posting" per user)
